16977b73dd37c2cd1ec1d927f2475773e87e7269
zackarychapple <zackarychapple30@gmail.com>
2023-04-04 11:54:02 -0400

    fix: rspack build of nestjs

--- rspack.config.js
+++ rspack.config.js
@@ -1,21 +1,15 @@
-const path = require('path');
-module.exports = (env,argv) =>  {
-  // console.log('env:',env)
-  // console.log('argv:',argv)
-  return {
-    context: __dirname,
-    entry: {
-        main: './dist/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist')
-    },
-    infrastructureLogging: {
-      debug: true
-    },
-    // target: "node",
-    // devServer: {
-    //   hot: true
-    // },
-  }
-}
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  entry: {
+    main: './src/main.ts',
+  },
+  externalsType: 'commonjs',
+  externals: {
+    '@nestjs/common': '@nestjs/common',
+    '@nestjs/core': '@nestjs/core',
+    'tslib': 'tslib',
+  },
+  target: 'node',
+};
