1f4d9bbf033beee0833dba9f66be04fe14919f02
Douglas Machado <douglas.machado@valor-software.com>
2023-04-24 17:42:00 -0300

    added environment config file

--- rspack.config.js
+++ rspack.config.js
@@ -12,4 +12,18 @@ module.exports = {
     'tslib': 'tslib',
   },
   target: 'node',
+  infrastructureLogging: {
+    debug: false
+  },
+  resolve: {
+    alias: {
+      // # Scenario 1
+      //  It works
+      // './environments/environment': '/..{absolutePathToTheApp}/nestapp/src/environments/environment.prod.ts',
+      //
+      // # Scenario 2 (relative path)
+      // it should work also but it doesn't 
+      './environments/environment.ts': './environments/environment.prod.ts',
+    }
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log("Environment.production:" + environment.production);
   await app.listen(3000);
 }
 bootstrap();
